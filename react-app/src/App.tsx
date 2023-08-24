985cd86934b3fab13d6d3e47830d228dcfbe5a4d
Sajal <sajal.moni@gmail.com>
2023-08-24 13:42:51 +0300

    Alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,28 @@
-import ListGroup from './components/ListGroup';
+// import ListGroup from './components/ListGroup';
 
+// function App() {
+//   let items = ['Kokkola', 'Turku', 'Helsinki', 'Vaasa', 'Oulu'];
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+import Alert from './components/Alert';
 function App() {
-  let items = ['Kokkola', 'Turku', 'Helsinki', 'Vaasa', 'Oulu'];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Hello world</span>
+      </Alert>
     </div>
   );
 }
-
 export default App;
