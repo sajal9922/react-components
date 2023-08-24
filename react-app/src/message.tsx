b7ed872ae876a01c9cf7154c51b350ed6e5a7c0a
Sajal <sajal.moni@gmail.com>
2023-08-24 09:41:26 +0300

    React app practice

---
+++ react-app/src/message.tsx
@@ -0,0 +1,10 @@
+
+// naming convention Pascal casing
+function Message(){
+  //JSX: JavaScript XML
+  const name = 'Sajal'
+  if(name)
+  return <h1> hello {name}</h1>;
+  return <h1> hello world</h1>;
+}
+export default Message;
